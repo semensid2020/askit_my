cc7574e2a6bdb107151e71599eb002be77478652
semensid2020 <semensid2020@gmail.com>
2022-10-26 19:18:01 +0700

    Lesson 4. Сообщения для пользователя и стили (bootstrap)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,6 +15,8 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     if @question.save
+      # Уведомление, отображаемое один раз (до перезагрузки страницы):
+      flash[:success] = "Question created!"
       redirect_to questions_path
     else
       render :new
@@ -28,6 +30,7 @@ class QuestionsController < ApplicationController
   def update
     @question = Question.find_by(id: params[:id])
     if @question.update(question_params)
+      flash[:success] = "Question updated!"
       redirect_to questions_path
     else
       render :edit
@@ -37,6 +40,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question = Question.find_by(id: params[:id])
     @question.destroy
+    flash[:success] = "Question deleted!"
     redirect_to questions_path
   end
 
