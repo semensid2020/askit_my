3b781a15a0bc548c58a2c265068dbf2fd106651e
semensid2020 <semensid2020@gmail.com>
2022-10-27 22:17:48 +0700

    Lesson 5. Добавлены answers (отношение one-to-many), добавлен консёрн для обработки ошибок (error_handling)

---
+++ app/controllers/concerns/error_handling.rb
@@ -0,0 +1,21 @@
+# Note:
+# Всякие rescue_from лучше убирать в отдельные модули, которые в RoR называются concerns
+
+module ErrorHandling
+  extend ActiveSupport::Concern
+
+  # Добавляет весь функционал ниже в класс, куда подключен этот модуль (консёрн)
+  included do
+    rescue_from ActiveRecord::RecordNotFound, with: :notfound
+
+    private
+
+    def notfound(exception)
+      # Note:
+      # Записывает возникшую ошибку в журнал ошибок Rails:
+      logger.warn(exception)
+
+      render file: 'public/404.html', status: :not_found, layout: false
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :questions
+  resources :questions do
+    # Вложенный маршрут для ответов, т.к. они относятся к вопросу
+    resources :answers, only: %i[create destroy]
+  end
 
   root 'pages#index'
 end
