4ab75b834c6731fa701ff3edfb0c1bc99a309c7e
semensid2020 <semensid2020@gmail.com>
2022-10-28 15:30:37 +0700

    Lesson 6. Добавлена форма редактирования ответа (answer). Добавлены хэлперы для заголовков HTML-страниц

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :questions do
     # Вложенный маршрут для ответов, т.к. они относятся к вопросу
-    resources :answers, only: %i[create destroy]
+    resources :answers, except: %i[new show]
   end
 
   root 'pages#index'
