9d252bab3fc3325509da147e12f7af03d4ae032c
semensid2020 <semensid2020@gmail.com>
2022-10-31 19:06:15 +0700

    7 Lesson. 10. Дефолтные партиалы kaminari (отображения кнопок постраничной навигации) доработаны с помощью компонента Bootstrap Pagination 11. Ответы на вопросы тоже разделены по страницам

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,7 +8,7 @@ class QuestionsController < ApplicationController
 
   def show
     @answer = @question.answers.build
-    @answers = @question.answers.order created_at: :desc
+    @answers = @question.answers.order(created_at: :desc).page(params[:page])
   end
 
   def new
