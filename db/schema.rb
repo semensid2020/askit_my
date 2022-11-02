b425a73502d2dbf4b9cd3a172104c7485dab1d92
semensid2020 <semensid2020@gmail.com>
2022-11-02 12:53:04 +0700

    9 Lesson. A. Для сущностей Questions и Answers выставлены проверки null: false (некоторых полей) теперь и на уровне БД. Имя теперь сделано необязательным полем при регистрации

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_01_122332) do
+ActiveRecord::Schema.define(version: 2022_11_02_043344) do
 
   create_table "answers", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2022_11_01_122332) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
