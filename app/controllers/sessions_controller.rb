0a9b744c5b52898be7c970832e3d412389488078
semensid2020 <semensid2020@gmail.com>
2022-11-02 22:37:03 +0700

    9 Lesson. G. В контроллере sessions создан экшн create (создание новой сессии), в том числе аутентификация в нем юзера по email и паролю.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render plain: params.to_yaml
+    user = User.find_by(email: params[:email])
+    #  TODO разобрать
+    # user& - если user -> nil, то сразу переходим в else.
+    if user&.authenticate(params[:password])
+      sign_in(user)
+      flash[:success] = "Welcome back, #{current_user.name_or_email}!"
+      redirect_to root_path
+    else
+      flash[:warning] = "Incorrect email and/or password!"
+      redirect_to new_session_path
+    end
   end
 
   def destroy
