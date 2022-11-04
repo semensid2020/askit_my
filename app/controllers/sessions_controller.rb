6229776179cce89fa2aed6b3d7077404fd11b098
semensid2020 <semensid2020@gmail.com>
2022-11-04 19:38:52 +0700

    9 Lesson. I. Добавлена проверка на то, что юзер авторизовался. Если это так, то ему не отображаются кнопки Sign Up и Log In (и соответствующие страницы недоступны). Кнопка же удаления сессии (логаут) наоборот требует чтобы юзер был авторизован. Также, для вьюшки логина (sessions/new) исправлен session_path (после предыдущего изменения путей в routes.rb)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
 
+  before_action :require_no_authentication, only: %i[new create]
+  before_action :require_authentication, only: :destroy
+
   def new
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :require_no_authentication
+
   def new
     @user = User.new
   end
