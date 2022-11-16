c82aae0281772d1f7cc619fa8b065dd8df400b0a
semensid2020 <semensid2020@gmail.com>
2022-11-16 19:06:19 +0700

    11 Lesson. B. Подготовка к доработке по импорту-экспорту Excel-файлов: - в Routes создано пространство имен admin - созданы соответствующие контроллер и вьюха, а также партиал к ней
    По URL-у /admin/users теперь доступен список всех пользователей

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Admin::UsersController < ApplicationController
+  before_action :require_authentication
+
+  def index
+    @pagy, @users = pagy(User.order(created_at: :desc))
+  end
+
+end
