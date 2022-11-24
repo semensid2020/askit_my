ba1e8652cfe6944510b7eb35b5d8152b58c6a619
semensid2020 <semensid2020@gmail.com>
2022-11-24 13:51:49 +0700

    13 Lesson. C. Интернационализация приложения гемом i18n: Добавляем переключение языков - Разделение на Локали добавлено в роуты с помощью скоупа locale - Добавлена проверка на то, поддерживается ли передаваемая при запросе локаль со стороны роутов (регулярное выражение) - Добавлены методы считывания запрашиваемой локали из передаваемого УРЛа при запросе: метод switch_locale для переключения локали и метод locale_from_url - проверки того, что локаль действительно поддерживается

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,23 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   include ErrorHandling
   include Authentication
+
+  around_action :switch_locale
+
+  private
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def locale_from_url
+    locale = params[:locale]
+    # Каждую локаль приведем к строке - .map(&:to_s):
+    # потому что строку и символ сравнивать нельзя
+    return locale if I18n.available_locales.map(&:to_s).include?(locale)
+    # Если запрошенную локаль мы не поддерживаем, тогда мы возвращаем просто nil
+    nil
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # resources - когда routes ожидает для некоторых маршрутов присутствие id-шника (видимо, когда сущностей м.б. много)
-  # resource  - если хотим, чтобы идентификаторов никаких не было.
-  resource :session, only: %i[new create destroy]
+  # Например: localhost/en/questions, при этом локаль НЕобязательна в УРЛе
+  scope "(:locale)", locale: /#{i18n.available_locales.join("|")}/ do
+    # resources - когда routes ожидает для некоторых маршрутов присутствие id-шника (видимо, когда сущностей м.б. много)
+    # resource  - если хотим, чтобы идентификаторов никаких не было.
+    resource :session, only: %i[new create destroy]
 
-  resources :users, only: %i[new create edit update]
+    resources :users, only: %i[new create edit update]
 
-  resources :questions do
-    # Вложенный маршрут для ответов, т.к. они относятся к вопросу
-    resources :answers, except: %i[new show]
-  end
+    resources :questions do
+      # Вложенный маршрут для ответов, т.к. они относятся к вопросу
+      resources :answers, except: %i[new show]
+    end
 
-  namespace :admin do
-    resources :users, only: %i[index create]
-  end
+    namespace :admin do
+      resources :users, only: %i[index create]
+    end
 
-  root 'pages#index'
+    root 'pages#index'
+
+  end #scope
+
 end
