b6d7f332dc330c6b0b81781707d3479b9467b63c
semensid2020 <semensid2020@gmail.com>
2022-12-07 22:54:18 +0700

    14 Lesson. D. Доработка по выводу автора вопроса/ответа 3: - Подключено взаимодействие с сервисом Gravatar. Первое отображение аватарок от этого сервиса

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -9,4 +9,11 @@ class UserDecorator < ApplicationDecorator
 
     email.split('@')[0]
   end
+
+  def gravatar(size: 30, css_class: '')
+    email_hash = Digest::MD5.hexdigest(email.strip.downcase)
+
+    h.image_tag("https://www.gravatar.com/avatar/#{email_hash}.jpg?s=#{size}",
+                class: "rounded #{css_class}", alt: name_or_email)
+  end
 end
