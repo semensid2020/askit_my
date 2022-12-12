350aed80611f5035932cd119f549662641804c3c
semensid2020 <semensid2020@gmail.com>
2022-12-12 22:21:11 +0700

    15 Lesson. H. Доработка по добавлению комментариев к вопросам и ответам 7: Остается такая проблема: если коммент написан с ошибкой (слишком короткий, например), то сообщение об ошибке корректно не отображается. Оно дублируется.
    Мы в _commentable.html.erb не можем понять в каком случае отображать сообщение об ошибке, а в каком нет (стр. 7).
    Поэтому на данном этапе эта ошибка отображается столько раз, сколько рендерится этот партиал,
    так как переменная @comment, она для контроллера "едина".
    
    Вариантов как решить это много. Можно было бы сделать это на уровне JS.
    Но мы попробуем обойтись без него на данном этапе:
    
    Будем смотреть, для какого типа объекта был оставлен этот комментарий @comment,
    и исходя из этой информации мы уже либо отображаем render shared/errors, либо нет.
    Кроме того, мы на основании этого можем либо раскрыть эту выпадающую штуку (блок с комментами), либо наоборот скрыть.
    
    иными словами: если писали коммент к вопросу, но он оказался коротким, то сообщение об ошибке и выпадающий блок с комментариями
    должен отображаться только под вопросом, а под каждым ответом - не должен отображаться (как есть сейчас).
    И наоборот.
    
    Как будем понимать, для чего был оставлен комментарий:
    - Прописана логика в декораторе комментариев (метод def for?)

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -3,4 +3,14 @@
 class CommentDecorator < ApplicationDecorator
   delegate_all
   decorates_association :user
+
+  def for?(commentable)
+    # Если штука была задекорирована, то нужно вытащить из неё конкретный объект,
+    # т.е. конкретный образец класса
+    commentable = commentable.object if commentable.decorated?
+    # Какая-то хитрая строка ^^^ . Разобраться...
+    commentable == self.commentable
+    # self в данном случае указывает на конкретный комментарий, и по связи commentable мы смотрим,
+    #  для чего конкретно комментарий был оставлен
+  end
 end
