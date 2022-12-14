712bcce0728e82ec0ceb789bfaf9632ea0dcefde
semensid2020 <semensid2020@gmail.com>
2022-12-14 12:13:44 +0700

    16 Lesson. A. Рефакторинг: доработан routes.rb Дублирующиеся маршруты resources :comments...   помещены в консёрн :commentable
    Также:
    доработан _menu.html.erb: использован url_for root_path, что в некоторых случаях позволяет избежать сброса на страницу 'Home' при смене языка.
                                                                                                                                             _05:09_

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  concern :commentable do
+    resources :comments, only: %i[create destroy]
+  end
+
   # Например: localhost/en/questions, при этом локаль НЕобязательна в УРЛе
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     # resources - когда routes ожидает для некоторых маршрутов присутствие id-шника (видимо, когда сущностей м.б. много)
@@ -9,18 +13,14 @@ Rails.application.routes.draw do
 
     resources :users, only: %i[new create edit update]
 
-    resources :questions do
+    resources :questions, concerns: :commentable do
       # Вложенный маршрут для ответов, т.к. они относятся к вопросу
       resources :answers, except: %i[new show]
-
-      resources :comments, only: %i[create destroy]
     end
 
     # Комменты к ответам пришлось выделить отдельно, чтобы более-менее удобный и не сильно длинный маршрут к ним был.
     # (поэтому вот такой дубль стр.14 появился ниже:)
-    resources :answers, except: %i[new show] do
-      resources :comments, only: %i[create destroy]
-    end
+    resources :answers, except: %i[new show], concerns: :commentable
 
     namespace :admin do
       resources :users, only: %i[index create]
