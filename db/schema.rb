ff5f5d23710f32aae24d86b2199347fc04869349
semensid2020 <semensid2020@gmail.com>
2022-12-20 17:45:48 +0700

    17 Lesson. C. Доработка по добавлению ролевой модели 1: - Создана миграция add_role_to_users, добавляющая поле role - В модель User.rb прописаны названия ролей, повешанные на enum
    Роль юзера можно проверять с помощью суффикса _role?
    user1.admin_role? => false						          _12:30_

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_14_093204) do
+ActiveRecord::Schema.define(version: 2022_12_20_102153) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -66,7 +66,9 @@ ActiveRecord::Schema.define(version: 2022_12_14_093204) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "remember_token_digest"
     t.string "gravatar_hash"
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["role"], name: "index_users_on_role"
   end
 
   add_foreign_key "answers", "questions"
