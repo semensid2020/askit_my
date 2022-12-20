6bbcdfb74a89c64637c8f4e96bb1d7f4c5df3a1a
semensid2020 <semensid2020@gmail.com>
2022-12-20 23:03:58 +0700

    17 Lesson. G. Доработка по добавлению ролевой модели 5: Осталось нам создать тот хэлпер-метод user_roles (а то мы его задействовали уже, но не создали сам метод). В этом методе мы хотим отрендерить все роли, которые у нас есть в принципе.
    Как сделаем?
    Берем только ключи ролей:  User.roles.keys  (т.е. basic, moderator, admin...)
    
    И далее мы выводим массив из двух элементов, потому что когда мы это дело будем рендерить в select'е,
    нам нужно указывать и текст, который увидет юзер (т.е. название роли), и ID-шник этой роли.
    (правда в качестве ID-шника все равно выступает англоязычное название:  value=basic, value=moderator...)
    
    Кстати, каждое название мы ещё и с большой буквы выводим с помощью метода titleize    _21:25_

---
+++ app/helpers/admin/users_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Admin
+  module UsersHelper
+    def user_roles
+      User.roles.keys.map do |role|
+        [ role.titleize, role ]
+      end
+    end
+  end
+end
