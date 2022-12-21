7349f4bd5fe15614260536dfa9f724bd9d0165c4
semensid2020 <semensid2020@gmail.com>
2022-12-21 23:06:22 +0700

    18 Lesson. C. Доработка по добавлению авторизации 3: На данном этапе при попытке создать вопрос получаем ошибку:   Pundit::NotAuthorizedError Давайте как-то её обработаем. - Доработали консерн authorization.rb, добавив rescue_from
    Т.е. при получении вышеуказанной ошибки будет выполняться метод user_not_authorized (см. файл)
    Таким образом, мы обработали ошибку.							      _12:03_

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -1,7 +1,21 @@
+# frozen_string_literal: true
+
 module Authorization
   extend ActiveSupport::Concern
 
   included do
     include Pundit
+
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+    private
+
+    def user_not_authorized
+      flash[:danger] = t('global.flash.not_authorized')
+      # Куда делается редирект:
+      # - либо туда, где юзер был до попытки выполнения действия (если request.referer есть)
+      # - если его нет, то просто на заглавную страницу
+      redirect_to(request.referer || root_path)
+    end
   end
 end
