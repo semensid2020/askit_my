de0adcbe68eb17efa68db0e76884169b1fd30276
semensid2020 <semensid2020@gmail.com>
2022-12-23 18:09:27 +0700

    18 Lesson. L. Доработка по добавлению авторизации 12: Наконец, разберемся с админским контроллером. В нём все будет просто - для всех экшнов одна и таже политика: user.admin_role? - Создана политика admin/user_policy.rb
    Теперь надо как-то эту политику применить.
    Для удобства создадим в папке /admin  базовый контроллер, и от него унаследуем уже имеющийся у нас .../admin/users_controller.rb
    Да и в целом от него будут наследоваться все админские контроллеры.
    - Создан ../admin/base_controller.rb
    - Доработан ../admin/users_controller.rb
    
    Таким образом, эта ^^^ админская политика применилась за счет передачи пространства имен :admin в методе def authorize в base_controller.rb
    
    Можно было бы так не делать (создавать контроллер-прокладку, наследовать...), но тогда пришлось бы каждый раз
    при вызове метода authorize передавать этот неймспейс :admin примерно в таком виде:
    authorize([:admin, (@user || User)])						  	   				         _32:17_

---
+++ app/policies/admin/base_controller.rb
@@ -0,0 +1,9 @@
+module Admin
+  class BaseController < ApplicationController
+    def authorize(record, query = nil)
+      # этот метод будет вызывать родительский метод, но также дописывать вот такую штуку :admin
+      # :admin - это как раз то пространство имен, где лежит наша политика.
+      super([:admin, record], query)
+    end
+  end
+end
