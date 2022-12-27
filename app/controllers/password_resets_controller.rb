08e80944618c63eae75404d6c91f305812f40ce9
semensid2020 <semensid2020@gmail.com>
2022-12-27 21:06:35 +0700

    19 Lesson. G. Доработка по добавлению сброса пароля УЗ 7: Теперь займемся методом UPDATE в контроллере. В этом методе нам соответственно нужно обновить пароль. - Создан метод def update, а также приватный метод user_params
    Но здесь возникает старая проблема, а именно: для того чтобы изменить пароль, нужно ввести старый пароль.
    
    А юзер естественно старый пароль не помнит.
    
    Поэтому давайте задействуем атрибут  :admin_edit, т.к. если он true, то старый пароль для смены не требуется.
    Добавили его в user_params тоже.
    
    Однако, и это ещё не все.  После того, как пароль был сброшен,
    нам по-видимому нужно очистить токен для сброса пароля и метку времени, когда инструкции сброса были отправлены.
    - Консёрн Recoverable дообогащен методом clear_reset_password_token, делающим это ^^^
    
    Теперь вопрос: на каком моменте этот метод нужно вызывать?
    самым элегантным будет использовать before_update
    
    На этом, наш новый функционал в принципе готов.							  _31:40_

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -24,7 +24,20 @@ class PasswordResetsController < ApplicationController
 
   def edit;end
 
+  def update
+    if @user.update(user_params)
+      flash[:success] =t('.success')
+      redirect_to new_session_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation).merge(admin_edit: true)
+  end
 
   def set_user
     redirect_to(new_session_path, flash: { warning: t('password_resets.fail') }) and return unless params[:user].present?
--- app/models/concerns/recoverable.rb
+++ app/models/concerns/recoverable.rb
@@ -4,10 +4,21 @@ module Recoverable
   extend ActiveSupport::Concern
 
   included do
+    # Вызывается перед обновлением записи, но после того как валидации прошли
+    # password_digest_changed - доступен, потому что в БД есть колонка password_digest
+    # :password_digest_changed? - был ли изменен digest в рамках текущей операции с БД
+    # т.е. если password_digestизменился, то сбрасываем данные о токене в clear_reset_password_token
+    before_update :clear_reset_password_token, if: :password_digest_changed?
+
     def set_password_reset_token
       # Для генерации токенов задействуем комбинацию уже имеющегося функционала def digest и SecureRandom
       update(password_reset_token: digest(SecureRandom.urlsafe_base64),
              password_reset_token_sent_at: Time.current)
+    end
+
+    def clear_reset_password_token
+      self.password_reset_token = nil
+      self.password_reset_token_sent_at = nil
     end
 
     def password_reset_period_valid?
